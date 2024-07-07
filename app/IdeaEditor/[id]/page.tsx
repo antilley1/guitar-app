66649358f6aafaf91a04405f74ebbedb4888eebd
antilley1 <mantilley1+github@gmail.com>
2024-07-07 13:52:16 -0500

    Pass Idea Id to IdeaEditor page

--- app/IdeaEditor/[id]/page.tsx
+++ app/IdeaEditor/[id]/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function IdeaEditorPage() {
+export default function IdeaEditorPage({ params }: { params: { id: string } }) {
     return(
         <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-base-100">
-      <h1 className="text-4xl font-bold mb-8 text-base-content">Idea Editor</h1>
+      <h1 className="text-4xl font-bold mb-8 text-base-content">Idea Editor for Idea #{params.id}</h1>
       <Link href="/">
         <button className="bg-primary hover:text-accent-content hover:bg-accent text-primary-content font-bold py-2 px-4 rounded mr-4 mb-4 w-full">Back</button>
       </Link>
