6d0d9b140dcac492d801db4dc4b871aaa56195d2
antilley1 <mantilley1+github@gmail.com>
2024-07-07 13:42:25 -0500

    Use new Id prop for navigation

--- app/components/GuitarIdea.tsx
+++ app/components/GuitarIdea.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 interface GuitarIdeaProps {
+    id: number;
     title: string;
     description: string;
     songExamples: string[]; //This would need to be a type Song that has a name, artist, key, etc
@@ -8,14 +9,14 @@ interface GuitarIdeaProps {
     // add any other properties you need
   }
   
-  export default function GuitarIdea({ title, description, songExamples }: GuitarIdeaProps) {
+  export default function GuitarIdea({ id, title, description, songExamples }: GuitarIdeaProps) {
     return (
       <div className="card bg-neutral text-neutral-content p-4 rounded-lg mb-4">
         <div className="card-body">
           <h2 className="card-title text-lg font-bold mb-2 text-center">{title}</h2>
           <p className="text-neutral-content">{description}</p>
           <p className="text-neutral-content italic">Examples: {songExamples}</p>
-          <Link href={`/IdeaEditor/${title}`}>
+          <Link href={`/IdeaEditor/${id}`}>
             <button className="bg-primary hover:text-secondary-content hover:bg-secondary text-primary-content font-bold py-2 px-4 rounded mr-4 mb-4 self-end">Edit Idea</button>
           </Link>
         </div>
--- app/components/IdeaFeed.tsx
+++ app/components/IdeaFeed.tsx
@@ -3,21 +3,25 @@ import GuitarIdea from "./GuitarIdea"; // Assuming GuitarIdea is the component f
 export default function IdeaFeed() {
     var guitarIdeas: any[] = [
       {
+        id: 1,
         title: "Slide up the 3rd string", 
         description: "Sliding up the G string from the 1st pentatonic position",
         songExamples: ["The Housefire"]
       }, 
       {
+        id: 2,
         title: "Third string major chord bend", 
         description: "Bending the G string up a whole step to create a major chord with the 1st and 2nd strings",
         songExamples: ["The Housefire"]
       }, 
       {
+        id: 3,
         title: "Third string major chord bend", 
         description: "Bending the G string up a whole step to create a major chord with the 1st and 2nd strings",
         songExamples: ["The Housefire"]
       }, 
       {
+        id: 4,
         title: "Third string major chord bend", 
         description: "Bending the G string up a whole step to create a major chord with the 1st and 2nd strings",
         songExamples: ["The Housefire"]
