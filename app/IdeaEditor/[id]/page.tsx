098b09f20abfe3a81edd81dc95a9c0d6f56d8052
antilley1 <mantilley1+github@gmail.com>
2024-08-15 20:22:44 -0500

    Style IdeaContent

--- app/IdeaEditor/[id]/page.tsx
+++ app/IdeaEditor/[id]/page.tsx
@@ -10,7 +10,9 @@ export default function IdeaEditorPage({ params }: { params: { id: string } }) {
 
     return(
       <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-base-100">
-        <IdeaContent id={ ideaId }/>
+        <div className='w-full max-w-5xl rounded-lg p-4 bg-neutral'>
+          <IdeaContent id={ ideaId }/>
+        </div>
         <CommentSection id={ ideaId }/>
         <NavigateIdeas ideaId={ ideaId }/>
       </main>
--- app/components/IdeaContent.tsx
+++ app/components/IdeaContent.tsx
@@ -3,10 +3,14 @@ import Commentary from './Commentary';
 
 export default function IdeaContent({ id }: { id: string }) {
     return (
-        <div className="text-lg p-4">
-          <h1>Idea</h1>
-          <Concept id={id}/>
-          <Commentary id={id}/>
+        <div className="text-lg p-4 mb-4">
+          <h1 className="text-xl font-bold mb-4">Idea</h1>
+          <div className="rounded-lg bg-primary p-4 mb-4 text-primary-content">
+            <Concept id={id}/>
+          </div>
+          <div className="rounded-lg bg-primary p-4 mb-4 text-primary-content">
+            <Commentary id={id}/>
+          </div>
         </div>
     );
 }
